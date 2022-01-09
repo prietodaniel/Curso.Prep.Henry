21087774f348649ab070c7fb4c45551c4f64de26
dPrieto <prietodaniel1@gmail.com>
2022-01-09 11:53:12 -0300

    Subo tarea 04

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -119,7 +119,20 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  var prod = 1;
+  if(arguments.length == 0){
+    return 0;
+  }
+  else if(arguments.length == 1){
+    return arguments[0];
+  }
+  else{
+    for ( i=0 ; i<arguments.length ; i++){
+      prod *= arguments[i]
+    }
+  }
 
+  return prod;
   
 }
 
@@ -127,7 +140,14 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
+  var suma = 0;
+  arreglo.forEach(element => {
+    if(element > 18){
+      suma++;
+    }
+  });
 
+  return suma;
 }
 
 
@@ -136,7 +156,12 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if(numeroDeDia == 1 || numeroDeDia == 7){
+    return "Es fin de semana"
+  }
+  else{
+    return "Es dia Laboral"
+  }
 } 
 
 
@@ -144,7 +169,12 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  if(String(n)[0] == "9"){
+    return true
+  } 
+  else{
+    return false
+  }
 }
 
 
@@ -152,7 +182,20 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  var valorAnterior = 0;
+  var primerValor = 1;
+  for(var i=0; i<arreglo.length; i++){
+    if(primerValor == 1){
+      valorAnterior = arreglo[i];
+      primerValor = 0;
+    }
+    else{
+      if(arreglo[i] !== valorAnterior){
+        return false;
+      }
+    }
+  }
+  return true;
 } 
 
 
@@ -161,6 +204,22 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var mesesABuscar = ["Enero", "Marzo", "Noviembre"];
+  var arregloNuevo = [];
+
+  for(var i=0; i < array.length; i++){
+    if(mesesABuscar.includes(array[i])){
+      arregloNuevo.push(array[i]);
+    }
+  }
+
+  if(arregloNuevo.length < 3){
+    return "No se encontraron los meses pedidos";
+  }
+  else{
+    return arregloNuevo;
+  }
+  
 }
 
 
@@ -168,6 +227,20 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var nuevoArray = [];
+
+  //for(var i=0; i<array.length; i++){
+   // if(array[i] > 100){
+    //  nuevoArray.push(array[i])
+    //}
+  //}
+
+  array.forEach(element => {
+    if(element > 100)
+      nuevoArray.push(element);
+  });
+
+  return nuevoArray;
 }
 
 
@@ -179,6 +252,18 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var i=0;
+  var arreglo = []
+  do{
+    numero += 2;
+    arreglo.push(numero);
+    if (numero == i){
+      return "Se interrumpió la ejecución";
+      break;
+    }
+  }while(++i < 10);
+
+  return arreglo;
 }
 
 
@@ -189,6 +274,18 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var i=0;
+  var arreglo = []
+  do{
+    if (i == 5){
+      continue;
+    }
+    numero += 2;
+    arreglo.push(numero);
+  }while(++i < 10);
+  
+  return arreglo;
+
 }
 
 
